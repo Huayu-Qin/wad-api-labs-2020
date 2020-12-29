951b8e3249ff4ce02c5b7516b1273e68df01296a
Huayu Qin <445670669@qq.com>
2020-12-29 20:57:35 +0000

    Topic 10 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,24 +1,24 @@
 import userModel from '../api/users/userModel';
 
 const users = [
-  {
-    'username': 'user1',
-    'password': 'test1',
-  },
-  {
-    'username': 'user2',
-    'password': 'test2',
-  },
+    {
+        'username': 'user1',
+        'password': 'test1',
+    },
+    {
+        'username': 'user2',
+        'password': 'test2',
+    },
 ];
 
 // deletes all user documents in collection and inserts test data
 export async function loadUsers() {
-  console.log('load user Data');
+    console.log('load user Data');
     try {
-      await userModel.deleteMany();
-      await userModel.collection.insertMany(users);
-      console.info(`${users.length} users were successfully stored.`);
+        await userModel.deleteMany();
+        await users.forEach(user => userModel.create(user));
+        console.info(`${users.length} users were successfully stored.`);
     } catch (err) {
-      console.error(`failed to Load user Data: ${err}`);
+        console.error(`failed to Load user Data: ${err}`);
     }
-  }
+}
